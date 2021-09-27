400bc7f140e009434df0dd5fcebc88e2da535550
Pablo Zinemanas <pablo.zinemanas@upf.edu>
2021-09-27 15:21:09 +0200

    Update util.py

--- util.py
+++ util.py
@@ -8,6 +8,17 @@ from plotly.offline import iplot
 import plotly.graph_objs as go
 from scipy import signal
 
+colors = ['#1f77b4',
+          '#ff7f0e',
+          '#2ca02c',
+          '#d62728',
+          '#9467bd',
+          '#8c564b',
+          '#e377c2',
+          '#7f7f7f',
+          '#bcbd22',
+          '#17becf']
+
 def load_audio(filepath):
   data, sr = sf.read(filepath)
 
